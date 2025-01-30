8fd508fb0e193092c5a657cf7a011c044999b196
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-01-30 19:40:04 +0530

    Time: 3 ms (45.93%), Space: 45.7 MB (69.81%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int reachable=0;
-        for(int i=0; i<nums.length; ++i){
+        for(int i=0; i<nums.length; i++){
             if(reachable <i){
                 return false;
             }
